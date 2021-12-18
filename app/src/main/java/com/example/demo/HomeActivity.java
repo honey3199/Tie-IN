98267d516cfc4ebc0fc10601d9327dd0dfe99116
honey193 <honeyben.shah@zopsmart.com>
2021-12-18 17:39:09 +0530

    navigation drawer addding

--- app/src/main/java/com/example/demo/HomeActivity.java
+++ app/src/main/java/com/example/demo/HomeActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.demo.navigation.NavigationFragment;
 import com.example.demo.views.SellerListFragment;
 import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationFragment.Callbacks {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
@@ -34,11 +38,20 @@ public class HomeActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
 
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        navigationView.setNavigationItemSelectedListener(item -> true);
+
+        NavigationFragment navigationFragment = NavigationFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.navigation_view, navigationFragment)
+                .commit();
+
+        /*ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, , toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+        actionBarDrawerToggle.syncState();*/
 
         FragmentTransaction fragmentTransaction =
                 getSupportFragmentManager().beginTransaction()
@@ -47,6 +60,24 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMasterItemClicked(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    /*@Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         FragmentTransaction fragmentTransaction;
         switch (item.getItemId()) {
@@ -77,5 +108,5 @@ public class HomeActivity extends AppCompatActivity {
                 return true;
         }
         return false;
-    }
+    }*/
 }
---
+++ app/src/main/java/com/example/demo/navigation/NavigationFragment.java
@@ -0,0 +1,87 @@
+package com.example.demo.navigation;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.demo.HomeActivity;
+import com.example.demo.R;
+import com.example.demo.SingInActivity;
+import com.example.demo.views.ProfileFragment;
+import com.example.demo.views.SellerListFragment;
+
+
+public class NavigationFragment extends Fragment {
+
+    private Callbacks callbacks;
+
+    public interface Callbacks {
+        void onMasterItemClicked(Fragment fragment);
+    }
+
+    TextView tvHome, tvAboutUs, tvContactUs, tvChangePassword, tvLogout;
+
+    public static NavigationFragment newInstance() {
+        return new NavigationFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.navigation_drawer, container, false);
+        tvHome = view.findViewById(R.id.tv_home);
+        tvAboutUs = view.findViewById(R.id.tv_about_us);
+        tvContactUs = view.findViewById(R.id.tv_contact_us);
+        tvChangePassword = view.findViewById(R.id.tv_change_password);
+        tvLogout = view.findViewById(R.id.tv_sign_out);
+
+        view.findViewById(R.id.navigation_header).setOnClickListener(v -> {
+            callbacks.onMasterItemClicked(ProfileFragment.newInstance());
+            /*FragmentTransaction fragmentTransaction = requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, ProfileFragment.newInstance());
+            fragmentTransaction.commit();*/
+        });
+
+        tvHome.setOnClickListener(v -> {
+            callbacks.onMasterItemClicked(ProfileFragment.newInstance());
+            /*FragmentTransaction fragmentTransaction = requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, SellerListFragment.newInstance());
+            fragmentTransaction.commit();*/
+        });
+
+        tvAboutUs.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, ProfileFragment.newInstance());
+            fragmentTransaction.commit();
+        });
+
+        tvContactUs.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, ProfileFragment.newInstance());
+            fragmentTransaction.commit();
+        });
+
+        tvChangePassword.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, ProfileFragment.newInstance());
+            fragmentTransaction.commit();
+        });
+
+        tvLogout.setOnClickListener(v -> {
+            Intent i = new Intent(requireActivity(), SingInActivity.class);
+            startActivity(i);
+            requireActivity().finish();
+        });
+
+        return view;
+    }
+}
